f6b020cd3ff93019f624b3ec31894c51663acd6a
Tevarone <tevarone@vpsorg.top>
2017-04-21 14:37:13 +0300

    Update Conversions.py

--- Conversions.py
+++ Conversions.py
@@ -1,30 +1,38 @@
-# -*- coding: utf-8
+# -*- coding: utf-8 -*-
 import os
-__developer__ = 'Tevarone'
-__date__ = '20/4/2017'
-''' sample conversions '''
-class Conversions:
-  def __init__(self):
-   swlf.input = raw_input('-')
-   os.system('title Conversion v1.0')
-   os.system('color fb')
-   print('CONVERSION V1').center(90)
-   print('Choose convert mode:\n[A]: From char to token\n[B]: From token to char')
-   x = self.input('Mode:')
-   if x == 'A':
-    self.convert(A)
-   elif x == 'B':
-    self.convert(B)
-	
-  def convert(self, mode):
-   if mode == 'A':
-    packet = ord(self.input('[CHAR]:'))
-    print('[TOKEN]: {0}'.format(str(packet)))
-    close(6)
-   elif mode == 'B':
-    packet = chr(self.input('[TOKEN]:'))
-    print('[CHAR]: {0}'.format(packet))
-    close(6)
-    
-if __name__ == '__main':
- Conversions()
+# Simple Conversion Type Script(Python)
+__developer__ = 'Tevar'
+__date__ = '20/4/2017' # Day/Month/Year
+class Conversion
+	def __init__(self):
+		title = 'CONVERSION V1.0' # Sub-Title script
+		message_start = 'Choose one only from convert modes:\n[A]: From char to token.\n[B]: From token to char.' # Message script starting
+		self.input = input('- ')
+		os.system('title Conversion v1.0')
+		os.system('color fb')
+		print(str(title)).center(90)
+		print(str(message_start))
+		x = self.input('Mode: ')
+		if x == 'A':
+			convert_start(A) 
+		elif x == 'B':
+			convert_start(B)
+		else:
+			print('Please choose A or B')
+			time(3)
+			close()
+
+	def convert_start(self, mode): # conversion start
+		__char__, __token__ = 'A','B'
+		if mode == __char__:
+			packet = ord(self.input('[CHAR]: '))
+			print('[TOKEN]: {0}'.format(packet))
+			time(6)
+			close()
+		elif mode == __token__:
+			packet = repr(self.input('[TOKEN]: '))
+			print('[CHAR]: {0}'.format(packet))
+			time(6)
+			close()
+if __name__ == '__main__':
+	Conversion()
